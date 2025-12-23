7d8dcb018a710927078c22da0bd90343fe328b84
Robert <r.niyonkuru@alustudent.com>
2025-12-23 16:55:17 +0200

    prisma debug, connection to models and auth intro

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [PrismaModule, AuthModule, UserModule, BookmarkModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,10 @@
 import { Module } from '@nestjs/common';
-@Module({})
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+@Module({
+  imports: [PrismaModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
 export class AuthModule {}
